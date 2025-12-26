1adc908fac41d38585719dbffc12c7dcdad0b154
weriomat <engel@weriomat.com>
2025-12-26 01:20:54 +0100

    feat: utilize the default trait

--- src/typesense/models/typesense_stats_model.rs
+++ src/typesense/models/typesense_stats_model.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::Map;
 use serde_json::Value;
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Default)]
 pub struct TypesenseStats {
     pub delete_latency_ms: f64,
     pub delete_requests_per_second: f64,
@@ -17,24 +17,4 @@ pub struct TypesenseStats {
     pub total_requests_per_second: f64,
     pub write_latency_ms: f64,
     pub write_requests_per_second: f64,
-}
-
-impl Default for TypesenseStats {
-    fn default() -> TypesenseStats {
-        TypesenseStats {
-            delete_latency_ms: 0.0,
-            delete_requests_per_second: 0.0,
-            import_latency_ms: 0.0,
-            import_requests_per_second: 0.0,
-            latency_ms: Map::new(),
-            overloaded_requests_per_second: 0.0,
-            pending_write_batches: 0.0,
-            requests_per_second: Map::new(),
-            search_latency_ms: 0.0,
-            search_requests_per_second: 0.0,
-            total_requests_per_second: 0.0,
-            write_latency_ms: 0.0,
-            write_requests_per_second: 0.0,
-        }
-    }
 }
